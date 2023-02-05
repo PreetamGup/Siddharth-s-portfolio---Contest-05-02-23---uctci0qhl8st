545fae324a659178a52a685b997b11a07f6766ee
Preetam Gupta <106731801+PreetamGup@users.noreply.github.com>
2023-02-05 19:57:15 +0530

    latest

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,8 +8,8 @@ const Home = () => {
             <h1>Home page</h1>
             <h2>Hi, my name is Siddharth </h2>
             <h2>and I work at <a href="https://www.newtonschool.co/">Newton School</a></h2>
-           <button id="home-to-blog"> Go to Blog</button>
-            <button id="home-to-info"> Go to Info</button>
+            <Link to='./Blog'> <button id="home-to-blog"> Go to Blog</button></Link>
+            <Link to='./Info'><button id="home-to-info"> Go to Info</button></Link>
 
         </div>
     )
